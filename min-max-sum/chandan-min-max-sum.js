294862e981a8500111162695fc8531cd2c32c31a
chandanghosh21 <chandanghosh21@gmail.com>
2019-04-25 02:51:23 +0600

    max,min and sum from array using class-obj

---
+++ min-max-sum/chandan-min-max-sum.js
@@ -0,0 +1,57 @@
+class MaxMin {
+    constructor(arr) {
+        console.log("Constructor initialized!");
+        this.arr = arr;
+    }
+    getMin() {
+        var min = this.arr[0];
+        for(var i=1; i<this.arr.length; i++) {  
+            if(min > this.arr[i]) {
+                min = this.arr[i];
+            }
+        }
+        return min;
+    }
+    getMax() {
+        var max = this.arr[0];
+        for(var i=1; i<this.arr.length; i++) {  
+            if(max < this.arr[i]) {
+                max = this.arr[i];
+            }
+        }
+        return max;
+    }
+}
+
+class Sum extends MaxMin  {
+    getSum() {
+    var sum = 0;
+    for(var i=0; i<this.arr.length; i++) {
+        sum += this.arr[i];
+     }
+     return sum;
+    }   
+}
+
+function getMaxOrMin(arr, option) {
+    var num = new Sum(arr);
+    switch(option.toUpperCase()) {
+        case 'MAX': 
+            value = num.getMax();
+        break;
+        case 'MIN':
+            value = num.getMin();
+        break;
+        case 'SUM':
+            value = num.getSum();
+        break;
+        default:
+            value = "Invalid option. Provider 'MIN' or 'MAX' or 'SUM'";
+        break;
+    }
+    return value;
+}
+
+var arr = [5,10,45,100,80,7,9,108,1,500];
+var value = getMaxOrMin(arr,'sum');
+console.log(value);
